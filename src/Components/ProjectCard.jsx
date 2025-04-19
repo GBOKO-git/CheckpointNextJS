5e24853dde6c072669255b0e12b6c75406209be9
GBOKO-git <gboko.amara@uvci.edu.ci>
2025-04-19 10:51:57 +0000

    responsive

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -13,27 +13,30 @@ const ProjectCard = ({project}) => {
                 <img className="rounded-t-xl md:w-80 md:h-50 xl:h-90 xl:w-120 max-sm:w-auto max-sm:h-80 font-semi-condensed  grid object-cover transition-transform duration-500 hover:scale-105 "
                 src={imgUrl} alt={name} />
                 </div>
-                <div className="bg-slate-50 h-82 w-auto grid p-2 rounded-b-2xl shadow-lg max-sm:font-bold max-lg:text-xl text-l font-semibold ">
-                    <h3 className="font-bold">{name}</h3>
+                <div className="bg-slate-50 h-82 w-auto grid p-2 rounded-b-2xl shadow-lg max-sm:font-bold max-lg:text-xl text-md font-semibold ">
+                    <h3 className="font-bold text-lg text-yellow-600 text-center">{name}</h3>
                     {/* Texte toujours visible en md+ */}
-                <p className="hidden md:block text-gray-700">
+                    <p className="hidden md:block text-black text-justify">
                     {description}</p>
-                    <p>{technologie} </p>
+                    <p className="text-center text-red-600">{technologie} </p>
+                    {/* <div className="flex justify-around"> */}
                     <div className="text-center">
-                        <button className="md:hidden bg-blue-400/50 text-xl p-2 rounded-l "
+                        <button className="md:hidden bg-blue-300 px-3  text-xl  rounded hover:text-amber-50 hover:bg-blue-900 hover:cursor-pointer"
                         onClick={() => setText(!text)}>
-                            {text ? "Masquer" : "Afficher le texte"}
+                            {text ? "Masquer" : "Description"}
                         </button>
                         
                         {text && 
                         (
-                            <p className="w-80 text-justify h-40 font-normal" >{description}</p>
+                            <p className="text-justify h-40 font-bold text-black text-sm" >{description}</p>
                         )}
+                        
                      </div>
-                    
+                     
                     <Link href={projectUrl}
                     className=" text-center text-xl font-serif" target="blank"
-                    ><SharedButton name="see more"/></Link>
+                    ><SharedButton name="voir plus"/></Link>
+                    
                 </div>
                 
             </div>
--- src/Components/ProjectList.jsx
+++ src/Components/ProjectList.jsx
@@ -3,7 +3,7 @@ const { default: ProjectCard } = require("./ProjectCard")
 const ProjectList = ({projects}) => {
     return(
         <>
-            <div className="grid max-sm:grid-cols-1  max-lg:grid-cols-2  max-sm:gap-5 max-sm:py-7 grid-cols-4  lg:gap-8 lg:p-6 max-lg:w-auto w-l bg-slate-100  place-items-center overflow-hidden  shadow-lg">
+            <div className="grid max-sm:grid-cols-1  max-lg:grid-cols-2  max-sm:gap-5 max-sm:py-7 grid-cols-4  lg:gap-8 lg:p-6 max-lg:w-auto w-l bg-gray-300  place-items-center overflow-hidden  shadow-lg">
                 {
                     projects?.map((project, index) =>(
                         <ProjectCard key={index} project={project}/>
